e11253c42dd2a4c200e8e3295024dbd35f50be1a
jeborunda <joseborunda@gmail.com>
2023-10-25 14:45:54 -0600

    update comments

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -8,7 +8,7 @@ var formSubmitHandler = function (event) {
   event.preventDefault();
 
   var query = foodInputEl.value.trim();
-  var cuisine = event.submitter.getAttribute('data-cuisine'); // Get cuisine from clicked button
+  var cuisine = event.submitter.getAttribute('data-cuisine'); // Get cuisine information     
 
   if (query || cuisine) {
     searchRecipes(query, cuisine);
@@ -29,17 +29,17 @@ var buttonClickHandler = function (event) {
 
 var searchRecipes = function (query, cuisine) {
   var apiUrl = 'https://api.spoonacular.com/recipes/complexSearch';
-  var apiKey = '2a90fe47e07f4eaf8fdb94eef7e059df'; // Replace with your Spoonacular API key
+  var apiKey = '2a90fe47e07f4eaf8fdb94eef7e059df'; // API Key
 
-  // Construct the API URL with query and cuisine parameters
+  // Building API key, to include addRecipeInformation
   apiUrl += '?apiKey=' + apiKey;
 
   if (query) {
-    apiUrl += '&query=' + query;
+    apiUrl += '&query=' + query + '&addRecipeInformation=true';
   }
 
   if (cuisine) {
-    apiUrl += '&cuisine=' + cuisine;
+    apiUrl += '&cuisine=' + cuisine + '&addRecipeInformation=true';
   }
 
   fetch(apiUrl)
@@ -50,6 +50,7 @@ var searchRecipes = function (query, cuisine) {
       throw new Error('Network response was not ok.');
     })
     .then(function (data) {
+      console.log(data);
       displayRecipes(data.results, query, cuisine);
     })
     .catch(function (error) {
@@ -69,6 +70,7 @@ var displayRecipes = function (recipes, query, cuisine) {
     var recipeEl = document.createElement('div');
     recipeEl.classList = 'list-item';
     recipeEl.textContent = recipes[i].title;
+
 
     recipeContainerEl.appendChild(recipeEl);
   }
